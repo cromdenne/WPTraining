ebd08443dc88a445faae0677ab67dd25de8d1787
Cory Romdenne <cory@romdenne.com>
2016-12-02 15:13:24 -0600

    Implementing the loop
    Removed hard-coded posts from content.php. Added code to pull posts from
    back-end using "The Loop".

--- wordpress/wp-content/themes/janus/index.php
+++ wordpress/wp-content/themes/janus/index.php
@@ -4,7 +4,20 @@
 
         <div class="col-sm-8 blog-main">
 
-            <?php get_template_part( 'content', get_post_format() ); ?>
+            <?php
+            if ( have_posts() ) : while ( have_posts() ) : the_post();
+
+                get_template_part( 'content', get_post_format() );
+
+            endwhile; endif;
+            ?>
+            
+            <nav>
+                <ul class="pager">
+                    <li><a href="#">Previous</a></li>
+                    <li><a href="#">Next</a></li>
+                </ul>
+            </nav>
             
         </div><!-- /.blog-main -->
         
