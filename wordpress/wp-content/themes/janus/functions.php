04ad02fbed468aca47b4a7283ac0d246f653b099
Cory Romdenne <cory@romdenne.com>
2016-12-06 13:38:38 -0600

    Dynamically load styles and scripts
    Created functions.php. Implemented logic to correctly link styles and
    scripts.

---
+++ wordpress/wp-content/themes/janus/functions.php
@@ -0,0 +1,13 @@
+<?php
+    
+    // Add scripts and stylesheets
+    function janus_scripts() {
+        wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.7');
+        wp_enqueue_style( 'blog', get_template_directory_uri() . '/css/blog.css' );
+        wp_enqueue_style( 'style', get_template_directory_uri() . '/css/style.css' );
+        wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '3.3.7', true );
+    }
+
+    add_action( 'wp_enqueue_scripts', 'janus_scripts' );
+
+?>
